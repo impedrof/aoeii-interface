c9d8477e1ad3e9d66620353c3ea0b076a8949b46
Pedro Freitas <pedrofreitas790@gmail.com>
2022-02-20 13:10:50 -0300

    Design do input para pesquisa da lista de civilizações e filtrando ao digitar.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -18,7 +19,8 @@ import { StateModule } from './state.module';
     BrowserModule,
     AppRoutingModule,
     StateModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [
     { provide: CivilizationRepo, useClass: CivilizationDataRepo },
--- src/app/interface/civilizations/civilizations.component.ts
+++ src/app/interface/civilizations/civilizations.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Select } from '@ngxs/store';
+import { Select, Store } from '@ngxs/store';
 import { Observable } from 'rxjs';
 import { CIVILIZATIONS } from 'src/app/application/states/civilizations/civilizations';
 import { CivilizationUseCase } from 'src/app/application/use-case/civilization.use-case';
@@ -8,20 +8,38 @@ import { Civilization } from 'src/app/domain/civilization';
 @Component({
   selector: 'app-civilizations',
   templateUrl: './civilizations.component.html',
-  styleUrls: ['./civilizations.component.css']
+  styleUrls: ['./civilizations.component.css'],
 })
 export class CivilizationsComponent implements OnInit {
-
   @Select(CIVILIZATIONS) civilizations$: Observable<Civilization[]> | undefined;
+
+  searchInput: string = '';
 
   civilizations: Civilization[] | undefined;
 
-  constructor(private civilizationUseCase: CivilizationUseCase) {
-  }
+  constructor(
+    private civilizationUseCase: CivilizationUseCase,
+    private store: Store
+  ) {}
 
   ngOnInit(): void {
-    this.civilizations$?.subscribe(civilizations => this.civilizations = civilizations);
+    this.civilizations$?.subscribe(
+      (civilizations) => (this.civilizations = civilizations)
+    );
     this.civilizationUseCase.findAll();
   }
 
-} 
+  search(): void {
+    if (this.searchInput.length >= 2) {
+      this.civilizations = this.civilizations?.filter((civ) => {
+        const searchInput = this.searchInput.toLocaleLowerCase();
+        return (
+          civ.name.toLocaleLowerCase().includes(searchInput) ||
+          civ.armyType.toLocaleLowerCase().includes(searchInput)
+        );
+      });
+    } else {
+      this.civilizations = this.store.selectSnapshot(CIVILIZATIONS);
+    }
+  }
+}
